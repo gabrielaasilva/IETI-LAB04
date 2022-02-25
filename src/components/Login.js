3dba8dfc238eb60145cfe80469e1cfbe783e1148
gabrielaasilva <anagabriela2700@gmail.com>
2022-02-24 23:02:32 -0500

    yap

--- src/components/Login.js
+++ src/components/Login.js
@@ -16,16 +16,16 @@ export default function Login(props) {
         //     <TextField id="outlined-basic" label="Outlined" variant="outlined" />
         // </Box>
         <Grid container style={{ justifyContent: 'center', alignItem: "center", height: "100vh"}}>
-            <Grid xs={12} md={12} lg={12} item style={{marginTop: '150px'}} >
+            <Grid xs={12} md={12} lg={12} item style={{marginTop: '200px'}} >
                 <label style={{ display: "block", margin: "10px" }}>Correo electrónico</label>
                 <TextField id="outlined-basic" label="Outlined" variant="outlined" />
             </Grid>
-            <Grid xs={12} md={12} lg={12} item style={{ marginTop: "20px" }} >
+            <Grid xs={12} md={12} lg={12} item style={{marginBottom: '250px'}} >
                 <label style={{ display: "block", margin: "10px" }}>Constraseña</label>
                 <TextField id="outlined-basic" label="Outlined" variant="outlined" />
             </Grid>
-            <Grid xs={12} md={12} lg={12} item >
-                <Button variant="contained" style={{ marginLeft: 'auto', marginRight: 'auto', marginTop: '20px' }}>Log In</Button>
+            <Grid xs={12} md={12} lg={12} item>
+                <Button variant="contained" style={{ marginLeft: 'auto', marginRight: 'auto', marginBottom: '1000px'}}>Log In</Button>10
             </Grid>
 
         </Grid>
