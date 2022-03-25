734e6adc68428c70d6ac2a0a20437741efdb2562
gabrielaasilva <anagabriela2700@gmail.com>
2022-03-24 21:40:03 -0500

    serviceWorker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorkerRegistration'; 
 
 ReactDOM.render( //Document Object Manager => renderizar componetes (clase, render(Node))
   <React.StrictMode> 
@@ -15,3 +16,4 @@ ReactDOM.render( //Document Object Manager => renderizar componetes (clase, rend
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.register();
