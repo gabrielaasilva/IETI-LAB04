9ad039ea1be57cba40f87f6079cbadefcb97b846
gabrielaasilva <anagabriela2700@gmail.com>
2022-03-31 22:20:10 -0500

    Base code

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
 import Home from './components/Home';
 import Login from './components/Login';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 import initialState from './utils';
 import themeReducer from './utils';
 import { ThemeContext } from './ThemeContext';
 import { UserContext } from './UserContext';
 
-
 export default function App() {
-  return (
-    <div className='App'> 
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login></Login>} />
-          <Route path="/home" element={<Home></Home>} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    const [state, dispatch] = useReducer(themeReducer, initialState);
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/login" element={<Login></Login>} />
+                    <Route path="/home" element={<Home></Home>} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
-
-const [state, dispatch] = useReducer(themeReducer, initialState);
